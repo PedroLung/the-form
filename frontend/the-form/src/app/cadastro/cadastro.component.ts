4ed7f93ab6d2ad81fe21e6dd9fb4524d67057f85
Arthur Do Nascimento <143120648+oartuu@users.noreply.github.com>
2024-11-28 18:56:27 -0300

    feat: iniciando projeto e criando form

---
+++ frontend/the-form/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-cadastro',
+  standalone: true,
+  imports: [NavBarComponent],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.css'
+})
+export class CadastroComponent {
+
+}
