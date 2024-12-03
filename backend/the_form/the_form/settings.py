bbae6173a4ff5660e58b20ce423fc94d5ee2e6d6
Ivisson Pereira <ivipnascimento@hotmail.com>
2024-12-02 22:00:01 -0300

    cors: CORS to stable the connection with frontend

--- backend/the_form/the_form/settings.py
+++ backend/the_form/the_form/settings.py
@@ -38,10 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'funcionario'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # Adicione no início
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +52,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",  # URL do Angular
 ]
 
 ROOT_URLCONF = 'the_form.urls'
