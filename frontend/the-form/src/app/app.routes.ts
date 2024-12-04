8bcd9738a6c3790f2fd25bc750045534ad390c15
Arthur Do Nascimento <143120648+oartuu@users.noreply.github.com>
2024-12-04 20:10:54 -0300

    feat: criando home e integrando toda a navegação do sistema

--- frontend/the-form/src/app/app.routes.ts
+++ frontend/the-form/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import{CadastroComponent} from './cadastro/cadastro.component'
 import {CadastradoComponent} from './cadastrado/cadastrado.component'
+import { HomeComponent } from './home/home.component';
 
 
 
 export const routes: Routes = [
-    { path: '', component: CadastroComponent },
+    { path: '', component: HomeComponent },
+    {path: 'cadastro', component: CadastroComponent},
     { path: 'cadastrado', component: CadastradoComponent }
+
 ];
--- frontend/the-form/src/app/cadastrado/cadastrado.component.ts
+++ frontend/the-form/src/app/cadastrado/cadastrado.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-cadastrado',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cadastrado.component.html',
   styleUrl: './cadastrado.component.css'
 })
